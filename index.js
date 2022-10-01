b3aeee6c70912452bb577842ac0cd3e9d24113b3
VinothRajaGanesan <vinothraja.ganesan@gmail.com>
2022-10-01 22:23:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 class Node{
-  constructor(){
-    this.head = null;
-    this.length = 0;
+ constructor(value){
+   this.value = value,
+   this.next = null
+ }
+}
+
+class LinkedList{
+  constructor(value){
+    var newNode = new Node(value)
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  addNode(value){
+     
   }
 }
 
-var ll = new Node();
+var ll = new LinkedList();
+ll.addNode(45)
 console.log(ll);
